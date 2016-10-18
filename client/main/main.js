de15bbd5db51d0da3847044f72f2b0aba6bda33b
sam sherman <samdsherman@gmail.com>
2016-10-17 19:31:07 -0700

    add golden image support

--- client/main/main.js
+++ client/main/main.js
@@ -1,16 +1,20 @@
 angular.module('hsgatherer.main', [])
 .controller('MainController', function($scope, $http) {
   $scope.advanced = false;
+
   $scope.update = function() {
     var params = '';
+    if ($scope.partial) {
+      params += '&partial=' + $scope.partial;
+    }
     if ($scope.name) {
       params += '&name=' + $scope.name;
     }
     if ($scope.type) {
       params += '&type=' + $scope.type;
     }
-    if ($scope.partial) {
-      params += '&partial=' + $scope.partial;
+    if ($scope.cost !== undefined) {
+      params += '&cost=' + $scope.cost;
     }
     console.log(params);
 
@@ -21,4 +25,13 @@ angular.module('hsgatherer.main', [])
       $scope.data = data.data;
     });
   };
+
+  $scope.reset = function() {
+    delete $scope.partial;
+    delete $scope.name;
+    delete $scope.type;
+    delete $scope.cost;
+    $scope.update();
+  };
+
 });
